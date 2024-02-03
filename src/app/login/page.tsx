dc019501935c7375b5a1ef3db1520a4bbb6690c6
Nitin Shukla <nitinshukla413@gmail.com>
2024-02-03 16:11:47 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h1 className="text-2xl font-semibold mb-4">Login</h1>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-sm font-medium text-gray-600">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="mt-1 p-2 w-full border rounded-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 p-2 w-full border rounded-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>SignupSignup</div>
+  )
+}
+
+export default Signup
