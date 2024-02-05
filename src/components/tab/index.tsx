d5ef342d2cd021c9a24e1f98f79db1b1e437ccac
Priyanka26012 <152428808+Priyanka26012@users.noreply.github.com>
2024-02-05 23:58:53 +0530

    header'

---
+++ src/components/tab/index.tsx
@@ -0,0 +1,53 @@
+import {
+  Avatar,
+  Icon,
+  IconButton,
+  SwipeableDrawer,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import { useState } from "react";
+const TabNavigator = ({
+  tabs,
+  getTabContent,
+}: {
+  tabs: string[];
+  getTabContent: (id: number) => any;
+}) => {
+  const [value, setValue] = useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  return (
+    <div className="flex flex-col justify-start w-full">
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="nav tabs example"
+        role="navigation"
+        className="w-full flex  justify-start border-b-2 "
+        TabIndicatorProps={{
+          style: { background: "#0444BB", height: "4px", color: "red" },
+        }}
+      >
+        {tabs?.map((elem: string, i: number) => (
+          <Tab
+            label={elem}
+            style={{
+              ...(value == i
+                ? {
+                    color: "#0444BB",
+                    backgroundColor: "rgb(192,192,192,0.15)",
+                  }
+                : ""),
+            }}
+            className="flex-[0.5] p-5"
+          />
+        ))}
+      </Tabs>
+      {getTabContent(value)}
+    </div>
+  );
+};
+
+export default TabNavigator;
