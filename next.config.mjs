b94020327f47d55e8df32b59b02823fd6bc19e6d
Nitin Shukla <nitinshukla413@gmail.com>
2024-02-11 12:30:08 +0530

    crasoule

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    image: {
+        domains: ['fakestoreapi.com'],
+    },
+};
 
 export default nextConfig;
